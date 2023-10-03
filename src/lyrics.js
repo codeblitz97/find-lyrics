6e2c3f0368ca151a406fc13525220877766b73b8
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-10-03 20:45:12 +0600

    Create baseRequest function for easier code

--- src/lyrics.js
+++ src/lyrics.js
@@ -42,11 +42,7 @@ const getTrack = async (query) => {
   return track;
 };
 
-/**
- *
- * @param {string} query - The search query
- */
-async function getLyrics(query) {
+const baseRequest = async (query) => {
   const headers = {
     authority: "apic-desktop.musixmatch.com",
     cookie: "x-mxm-token-guid=",
@@ -74,8 +70,26 @@ async function getLyrics(query) {
   const finalURL = baseURL + params.toString();
 
   try {
-    const response = await axios.get(finalURL, { headers: headers }); // Make the HTTP request using Axios
+    const response = await axios.get(finalURL, { headers: headers });
     const body = response.data.message.body.macro_calls;
+
+    return body;
+  } catch (error) {
+    console.error("Axios request error:", error);
+    return {
+      error: "An error occurred while fetching lyrics.",
+      uri: info.uri,
+    };
+  }
+};
+
+/**
+ *
+ * @param {string} query - The search query
+ */
+async function getLyrics(query) {
+  try {
+    const body = await baseRequest(query);
 
     return body["track.lyrics.get"].message.body.lyrics.lyrics_body;
   } catch (error) {
@@ -92,35 +106,8 @@ async function getLyrics(query) {
  * @param {string} query - The search query
  */
 async function getSyncedLyrics(query) {
-  const headers = {
-    authority: "apic-desktop.musixmatch.com",
-    cookie: "x-mxm-token-guid=",
-  };
-
-  const info = await getTrack(query);
-  const baseURL = `https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get?format=json&namespace=lyrics_richsynched&subtitle_format=mxm&app_id=web-desktop-app-v1.0&`;
-
-  const durr = info.duration / 1000;
-
-  const params = new URLSearchParams();
-
-  params.append("q_album", info.album);
-  params.append("q_artist", info.artist);
-  params.append("q_artists", info.artist);
-  params.append("q_track", info.title);
-  params.append("track_spotify_id", info.uri);
-  params.append("q_duration", durr);
-  params.append("f_subtitle_length", Math.floor(durr));
-  params.append(
-    "usertoken",
-    "200501593b603a3fdc5c9b4a696389f6589dd988e5a1cf02dfdce1"
-  );
-
-  const finalURL = baseURL + params.toString();
-
   try {
-    const response = await axios.get(finalURL, { headers: headers }); // Make the HTTP request using Axios
-    const body = await response.data.message.body.macro_calls;
+    const body = await baseRequest(query);
 
     const subStri = await body["track.subtitles.get"].message.body
       .subtitle_list[0].subtitle.subtitle_body;
